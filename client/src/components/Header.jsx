53d58f6294cba0a79472c6df644d1395dc72ef98
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-07-17 13:49:28 +0200

    Working on Navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,40 @@
+import { Link } from 'react-router-dom';
+import { AiFillCaretDown, AiOutlineMenu } from 'react-icons/ai';
+import { useState } from 'react';
+
+import "./styles/Header.css";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="navbar"></nav>
+    <nav className="navbar">
+      <div className='desktop'>
+        <Link to={"/o-nama"}>O nama</Link>
+        <div className='dropdown'>
+          <Link to={"/nasi-proizvodi"} className='dropbtn'>Nasi proizvodi <AiFillCaretDown /></Link>
+          <div className='dropdown-content'>
+            <Link to={"/nasi-proizvodi/klub-stolovi"}>Klub Stolovi</Link>
+            <Link to={"/nasi-proizvodi/trpezarijski-stolovi"}>Trpezarijski Stolovi</Link>
+            <Link to={"/nasi-proizvodi/komode"}>Komode</Link>
+            <Link to={"/nasi-proizvodi/industrijski-stolovi"}>Industrijski Stolovi</Link>
+            <Link to={"/nasi-proizvodi/biblioteka-polica"}>Biblioteka Polica</Link>
+            <Link to={"/nasi-proizvodi/stepenista"}>Stepenista</Link>
+            <Link to={"/nasi-proizvodi/ostalo"}>Ostalo</Link>
+          </div>
+        </div>
+        <Link to={"/materijali"}>Materijali</Link>
+        <Link to={"/kontakt"}>Kontakt</Link>
+      </div>
+
+      <div className='mobile'>
+        <div className='menubtn'>
+          <AiOutlineMenu />
+        </div>
+        {isOpen ? 
+          <div></div> : <div></div> 
+        }
+      </div>
+    </nav>
   )
 }
